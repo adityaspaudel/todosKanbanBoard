da266099d687e64bc468f7e79e90eb2439adeed5
Aaditya Paudel <adityaspaudel@gmail.com>
2026-01-06 10:16:39 +0545

    taskSchema and model defined

---
+++ server/src/models/taskModel.js
@@ -0,0 +1,52 @@
+const mongoose = require("mongoose");
+
+const taskSchema = new mongoose.Schema(
+	{
+		title: {
+			type: String,
+			required: true,
+			trim: true,
+			maxlength: 150,
+		},
+
+		description: {
+			type: String,
+			trim: true,
+			maxlength: 1000,
+			default: "",
+		},
+
+		status: {
+			type: String,
+			enum: ["todo", "new", "doing", "done"],
+			default: "todo",
+			index: true, 
+		},
+
+		order: {
+			type: Number,
+			required: true,
+			min: 0,
+			index: true, 
+		},
+
+		userId: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: "User",
+			required: true,
+			index: true, 
+		},
+	},
+	{
+		timestamps: true,
+
+		indexes: [
+			{
+				fields: { userId: 1, status: 1, order: 1 },
+				options: { unique: true },
+			},
+		],
+	}
+);
+
+module.exports = mongoose.model("Task", taskSchema);
